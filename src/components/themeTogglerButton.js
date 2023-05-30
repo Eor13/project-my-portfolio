e4a51c43a77694c89a0508bb77c732833db0efb4
lucaoskaique <lucas.kaique175@hotmail.com>
2023-05-30 09:44:16 -0300

    fix: fixed button to change theme

--- src/components/themeTogglerButton.js
+++ src/components/themeTogglerButton.js
@@ -1,11 +1,17 @@
 import React, { useContext } from "react";
-import { ThemeContext, themes } from "../services/themeContext";
+import { ThemeContext } from "../services/themeContext";
 import { TogglerButton } from "./buttons";
 
+// 
+const ThemeTogglerButton = () => {
+    const { theme, updateTheme } = useContext(ThemeContext);
 
-const ThemeTogglerButton =() =>{
-    const { theme, upDateTheme } = useContext( ThemeContext )
-    return <TogglerButton onClick={() => upDateTheme(theme === themes.dark ? themes.light : themes.dark)}/>
+    const toggleTheme = () => {
+        const newTheme = theme === 'dark' ? 'light' : 'dark';
+        updateTheme(newTheme);
+    }
+
+    return <TogglerButton onClick={toggleTheme} />;
 };
 
-export {ThemeTogglerButton};
+export { ThemeTogglerButton };
